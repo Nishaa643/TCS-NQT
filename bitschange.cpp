67e52fe3fe64bbaa4b4c7f52af36b4aff4598340
Nishaa643 <nisha.y2400@gmail.com>
2026-05-19 16:49:00 +0530

    bits change manipulation

---
+++ bitschange.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int k = (1 << ((int)floor(log2(n)) + 1)) - 1;
+
+    cout << (n ^ k);
+
+    return 0;
+}
+
+
